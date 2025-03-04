ccc84e4e288535528782a9baff871446f17ce6ed
jun <somang567@naver.com>
2025-03-04 14:27:32 +0900

    supermarket Service Starting test

---
+++ src/main/java/com/martsystem/commonmartsystem/CommonMartSystemApplication.java
@@ -0,0 +1,13 @@
+package com.martsystem.commonmartsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CommonMartSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CommonMartSystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/martsystem/commonmartsystem/CommonMartSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.martsystem.commonmartsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CommonMartSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
